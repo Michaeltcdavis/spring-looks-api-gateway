72ae272e09349965e5fbca548fc606b96cc4efb0
Michael Davis <michaeltcdavis@gmail.com>
2026-03-21 14:18:37 -0400

    feat: use Azure Entra ID for authentication

--- src/main/java/com/springlooks/gateway/config/SecurityConfig.java
+++ src/main/java/com/springlooks/gateway/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.springlooks.gateway.config;
 
+import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadResourceServerHttpSecurityConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -19,15 +20,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+        return httpSecurity
+                .with(AadResourceServerHttpSecurityConfigurer.aadResourceServer(), Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(freeResourceUrls)
                         .permitAll()
                         .anyRequest()
                         .authenticated())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -38,3 +41,5 @@ public class SecurityConfig {
         return source;
     }
 }
+
+
